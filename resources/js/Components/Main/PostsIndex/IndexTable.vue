d9962126550cf4a453240c3fbed268e2a0ff8586
vsolo <victordev@mail.ru>
2025-05-22 23:44:50 +0300

    PC Оформить: 1) IndexTable (в работе) 2) Swiper in show 3) Оформить show 4) Поиск 5) Фильтрация (в работе)

--- resources/js/Components/Main/PostsIndex/IndexTable.vue
+++ resources/js/Components/Main/PostsIndex/IndexTable.vue
@@ -8,29 +8,56 @@ const props = defineProps({
     <div class="flex flex-col">
         <div class="-m-1.5 overflow-x-auto">
             <div class="p-1.5 min-w-full inline-block align-middle">
-                <div class="overflow-hidden">
-                    <table class="min-w-full divide-y divide-gray-200">
-                        <thead>
+                <div class="overflow-hidden rounded-t-lg">
+                    <table class="min-w-full divide-y divide-gray-200 ">
+                        <thead class=" ">
                         <tr>
-                            <th scope="col" class="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase">Бренд</th>
-                            <th scope="col" class="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase">Модель</th>
-                            <th scope="col" class="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase">Год</th>
-                            <th scope="col" class="px-6 py-3 text-end text-xs font-medium text-gray-500 uppercase">VIN</th>
-                            <th scope="col" class="px-6 py-3 text-end text-xs font-medium text-gray-500 uppercase">Цена</th>
-                            <th scope="col" class="px-6 py-3 text-end text-xs font-medium text-gray-500 uppercase">Ставка</th>
-                            <th scope="col" class="px-6 py-3 text-end text-xs font-medium text-gray-500 uppercase">Пробег</th>
-                            <th scope="col" class="px-6 py-3 text-end text-xs font-medium text-gray-500 uppercase">Коробка</th>
+                            <th scope="col" class="px-4 py-3 text-start text-xs font-medium text-gray-500 uppercase">
+                                Бренд
+                            </th>
+                            <th scope="col" class="px-4 py-3 text-start text-xs font-medium text-gray-500 uppercase">
+                                Модель
+                            </th>
+                            <th scope="col" class="px-4 py-3 text-start text-xs font-medium text-gray-500 uppercase">
+                                Год
+                            </th>
+                            <th scope="col" class="px-4 py-3 text-start text-xs font-medium text-gray-500 uppercase">VIN
+                            </th>
+                            <th scope="col" class="px-4 py-3 text-start text-xs font-medium text-gray-500 uppercase">
+                                Цена
+                            </th>
+                            <th scope="col" class="px-4 py-3 text-start text-xs font-medium text-gray-500 uppercase">
+                                Ставка
+                            </th>
+                            <th scope="col" class="px-4 py-3 text-start text-xs font-medium text-gray-500 uppercase">
+                                Пробег
+                            </th>
+                            <th scope="col" class="px-4 py-3 text-start text-xs font-medium text-gray-500 uppercase">
+                                Трансмиссия
+                            </th>
+                            <th scope="col"
+                                class="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase"></th>
                         </tr>
                         </thead>
 
                         <tbody class="divide-y divide-gray-200">
-                        {{post}}
-                        <tr v-for="post in posts" class="hover:bg-gray-100">
-                            <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-800">{{ post.brand }}</td>
-                            <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-800">45</td>
-                            <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-800">New York No. 1 Lake Park</td>
-                            <td class="px-6 py-4 whitespace-nowrap text-end text-sm font-medium">
-                                <button type="button" class="inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent text-blue-600 hover:text-blue-800 focus:outline-hidden focus:text-blue-800 disabled:opacity-50 disabled:pointer-events-none">Delete</button>
+
+                        <tr v-for="post in posts.data" class="hover:bg-gray-100">
+                            <td class="px-4 py-4 whitespace-nowrap text-sm font-medium text-gray-800">
+                                {{ post.brand }}
+                            </td>
+                            <td class="px-4 py-4 whitespace-nowrap text-sm text-gray-800">{{ post.model }}</td>
+                            <td class="px-4 py-4 whitespace-nowrap text-sm text-gray-800">{{ post.year_release }}</td>
+                            <td class="px-4 py-4 whitespace-nowrap text-sm text-gray-800">{{ post.vin }}</td>
+                            <td class="px-4 py-4 whitespace-nowrap text-sm text-gray-800">{{ post.price }}</td>
+                            <td class="px-4 py-4 whitespace-nowrap text-sm text-gray-800">{{ post.betce }}</td>
+                            <td class="px-4 py-4 whitespace-nowrap text-sm text-gray-800">{{ post.mileage }}</td>
+                            <td class="px-4 py-4 whitespace-nowrap text-sm text-gray-800">{{ post.transmission }}</td>
+                            <td class="px-4 py-4 whitespace-nowrap text-end text-sm font-medium">
+                                <button type="button"
+                                        class="inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent text-blue-600 hover:text-blue-800 focus:outline-hidden focus:text-blue-800 disabled:opacity-50 disabled:pointer-events-none">
+                                    Delete
+                                </button>
                             </td>
                         </tr>
                         </tbody>
